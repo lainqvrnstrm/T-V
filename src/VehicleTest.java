77b7d6c2877c4dfbe002ab4a9f9f911409c0b761
Pontus <pontus.laestadius@gmail.com>
2018-01-30 11:01:52 +0100

    adds default tests

---
+++ src/VehicleTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveForward() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void leftLaneDetect() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeLane() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void whereIs() {
+    }
+}
