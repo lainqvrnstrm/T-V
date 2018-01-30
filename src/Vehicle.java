18670bf00a7f56bded6abcca61c9cc52ff22fdcc
Pontus <pontus.laestadius@gmail.com>
2018-01-30 10:56:06 +0100

    init classes

---
+++ src/Vehicle.java
@@ -0,0 +1,18 @@
+public class Vehicle {
+
+    public boolean moveForward(Gyro position) {
+        throw new Error("Not Implemented.");
+    }
+
+    public boolean leftLaneDetect(Radar[] radars, Lidar lidar) {
+        throw new Error("Not Implemented.");
+    }
+
+    public boolean changeLane(Radar[] radars, Lidar lidar) {
+        throw new Error("Not Implemented.");
+    }
+
+    public Gyro whereIs() {
+        throw new Error("Not Implemented.");
+    }
+}
