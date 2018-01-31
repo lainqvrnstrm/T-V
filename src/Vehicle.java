6e1367435f868d62c97c091ee784923e28909f72
Pontus <pontus.laestadius@gmail.com>
2018-01-31 15:38:53 +0100

    adds cmments

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -5,8 +5,14 @@ public class Vehicle {
         gyro = new Gyro();
     }
 
+    /**
+     * Moves the car 5 meters forward in the longitude position.
+     * @return If the car's position has changed or not.
+     */
     public boolean moveForward() {
+        // Increments the longitude of the gyro to simulate moving forward.
         this.gyro.longitude += 5;
+        // Returns true because incrementing the longitude moves the car. Returning true indicates a change of longitude.
         return true;
     }
 
